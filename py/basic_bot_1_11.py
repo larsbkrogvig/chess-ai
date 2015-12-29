c6607192e3466bdd7a74ce8bdf93b48d80def9e2
larsbkrogvig <larsbkrogvig@gmail.com>
2015-12-29 23:37:56 +0100

    No more draws

--- py/basic_bot_1_11.py
+++ py/basic_bot_1_11.py
@@ -12,7 +12,7 @@ from collections import defaultdict
 debug = True
 
 def name():
-    return "basic-1.11"
+	return "basic-1.11"
 
 
 # # Parameters
@@ -22,34 +22,34 @@ def name():
 r = 0.05
 
 value_piece = {
-    chess.PAWN:   1.0,
-    chess.KNIGHT: 2.95,
-    chess.BISHOP: 3.05,
-    chess.ROOK:   5.0,
-    chess.QUEEN:  9.0,
-    chess.KING:   0.0
+	chess.PAWN:   1.0,
+	chess.KNIGHT: 2.95,
+	chess.BISHOP: 3.05,
+	chess.ROOK:   5.0,
+	chess.QUEEN:  9.0,
+	chess.KING:   0.0
 }
 
 # Position weight
 wp = [[1,1,1,1,1,1,1,1],
-      [1,2,2,2,2,2,2,1],
-      [1,2,3,3,3,3,2,1],
-      [1,2,3,5,5,3,2,1],
-      [1,2,3,5,5,3,2,1],
-      [1,2,3,3,3,3,2,1],
-      [1,2,2,2,2,2,2,1],
-      [1,1,1,1,1,1,1,1]]
+	  [1,2,2,2,2,2,2,1],
+	  [1,2,3,3,3,3,2,1],
+	  [1,2,3,5,5,3,2,1],
+	  [1,2,3,5,5,3,2,1],
+	  [1,2,3,3,3,3,2,1],
+	  [1,2,2,2,2,2,2,1],
+	  [1,1,1,1,1,1,1,1]]
 WP = sum(map(sum, wp))
 
 # Attack weight (for black)
 wa0 = [[3,4,4,4,4,4,4,3],
-       [3,4,4,4,4,4,4,3],
-       [2,3,3,3,3,3,3,2],
-       [2,3,3,3,3,3,3,2],
-       [1,2,2,2,2,2,2,1],
-       [1,2,2,2,2,2,2,1],
-       [1,1,1,1,1,1,1,1],
-       [1,1,1,1,1,1,1,1]]
+	   [3,4,4,4,4,4,4,3],
+	   [2,3,3,3,3,3,3,2],
+	   [2,3,3,3,3,3,3,2],
+	   [1,2,2,2,2,2,2,1],
+	   [1,2,2,2,2,2,2,1],
+	   [1,1,1,1,1,1,1,1],
+	   [1,1,1,1,1,1,1,1]]
 WA = sum(map(sum, wa0))
 wa = {}
 wa[chess.WHITE] = wa0[::-1]
@@ -57,113 +57,116 @@ wa[chess.BLACK] = wa0
 
 
 square_weight = {'pos': {},
-                 'atk': {chess.WHITE: {},
-                         chess.BLACK: {}}
-                 }
+				 'atk': {chess.WHITE: {},
+						 chess.BLACK: {}}
+				 }
 for i in xrange(8):
-    for j in xrange(8):
-        square_weight['pos'][chess.square(j,i)] = wp[i][j]/float(WP)
-        square_weight['atk'][chess.WHITE][chess.square(j,i)] = wa[chess.WHITE][i][j]/float(WA)
-        square_weight['atk'][chess.BLACK][chess.square(j,i)] = wa[chess.BLACK][i][j]/float(WA)
+	for j in xrange(8):
+		square_weight['pos'][chess.square(j,i)] = wp[i][j]/float(WP)
+		square_weight['atk'][chess.WHITE][chess.square(j,i)] = wa[chess.WHITE][i][j]/float(WA)
+		square_weight['atk'][chess.BLACK][chess.square(j,i)] = wa[chess.BLACK][i][j]/float(WA)
 
 def eval_board(board):
 
-    score = {chess.WHITE: {'material': 0, 'position': 0, 'attack': 0},
-             chess.BLACK: {'material': 0, 'position': 0, 'attack': 0}}
-    for s in chess.SQUARES:
-        p = board.piece_at(s)
-        if p:
-            score[p.color]['material'] += value_piece[p.piece_type]
-            score[p.color]['position'] += square_weight['pos'][s]
-        if board.is_attacked_by(chess.WHITE, s):
-            score[chess.WHITE]['attack'] += square_weight['atk'][chess.WHITE][s]
-        if board.is_attacked_by(chess.BLACK, s):
-            score[chess.BLACK]['attack'] += square_weight['atk'][chess.BLACK][s]
+	if root_board.is_game_over():
+		if root_board.is_checkmate():
+			return -1 * (int(root_board.turn)-int(not root_board.turn)) * float('inf')
+		else:
+			return 0
 
-    score_WHITE = score[chess.WHITE]['material'] + \
-                  (1-r) * score[chess.WHITE]['position'] + \
-                  r * score[chess.WHITE]['attack']
-    score_BLACK = score[chess.BLACK]['material'] + \
-                  (1-r) * score[chess.BLACK]['position'] + \
-                  r * score[chess.BLACK]['attack']
+	score = {chess.WHITE: {'material': 0, 'position': 0, 'attack': 0},
+			 chess.BLACK: {'material': 0, 'position': 0, 'attack': 0}}
+	for s in chess.SQUARES:
+		p = board.piece_at(s)
+		if p:
+			score[p.color]['material'] += value_piece[p.piece_type]
+			score[p.color]['position'] += square_weight['pos'][s]
+		if board.is_attacked_by(chess.WHITE, s):
+			score[chess.WHITE]['attack'] += square_weight['atk'][chess.WHITE][s]
+		if board.is_attacked_by(chess.BLACK, s):
+			score[chess.BLACK]['attack'] += square_weight['atk'][chess.BLACK][s]
 
-    return score_WHITE - score_BLACK
+	score_WHITE = score[chess.WHITE]['material'] + \
+				  (1-r) * score[chess.WHITE]['position'] + \
+				  r * score[chess.WHITE]['attack']
+	score_BLACK = score[chess.BLACK]['material'] + \
+				  (1-r) * score[chess.BLACK]['position'] + \
+				  r * score[chess.BLACK]['attack']
+
+	return score_WHITE - score_BLACK
 
 def move(board, show=False):
-    tree = build_tree(board)
-    result = parse_tree(tree)
-    #if debug:
-    #  for r in result:
-    #    print r
-    return result[0].pop(0)
+	tree = build_tree(board)
+	result = parse_tree(tree)
+	#if debug:
+	#  for r in result:
+	#    print r
+	return result[0].pop(0)
 
 def build_tree(root_board, halfmove_depth=1, limit_halfmove_depth=2):
 
-    if halfmove_depth == limit_halfmove_depth:
-        leaf = []
-        for move in root_board.legal_moves:
-            root_board.push(move)
-            fen = root_board.fen()
-            root_board.pop()
-            leaf.append((move, fen))
-        return {root_board.fen(): leaf}
-    else:
-        branch = []
-        for move in root_board.legal_moves:
-            root_board.push(move)
-            subbranch = build_tree(root_board, 
-                                   halfmove_depth=halfmove_depth+1,
-                                   limit_halfmove_depth=limit_halfmove_depth)
-            root_board.pop()
-            branch.append((move,subbranch))
-        return {root_board.fen(): branch}
-    pass
+	if halfmove_depth == limit_halfmove_depth:
+		leaf = []
+		for move in root_board.legal_moves:
+			root_board.push(move)
+			fen = root_board.fen()
+			root_board.pop()
+			leaf.append((move, fen))
+		return {root_board.fen(): leaf}
+	else:
+		branch = []
+		for move in root_board.legal_moves:
+			root_board.push(move)
+			subbranch = build_tree(root_board, 
+								   halfmove_depth=halfmove_depth+1,
+								   limit_halfmove_depth=limit_halfmove_depth)
+			root_board.pop()
+			branch.append((move,subbranch))
+		return {root_board.fen(): branch}
+	pass
 
 def parse_tree(tree):
 
-    ans = _parse_tree(tree)
-    return (ans[1:][::-1],ans[0])
+	ans = _parse_tree(tree)
+	return (ans[1:][::-1],ans[0])
 
 def _parse_tree(tree, depth=0):
 
-    root_board = chess.Board(tree.keys()[0])
-    sign = int(root_board.turn) - int(not root_board.turn)
+	root_board = chess.Board(tree.keys()[0])
+	sign = int(root_board.turn) - int(not root_board.turn)
 
-    movelist = tree.values()[0]
+	movelist = tree.values()[0]
 
-    if movelist:
+	if movelist:
 
-        if type(movelist[0][1])==str:
-            sys.stdout.flush()
+		if type(movelist[0][1])==str:
+			sys.stdout.flush()
 
-            leaf = []
-            for (move,fen) in movelist:
-                root_board.push(move)
-                evl = eval_board(root_board)
-                root_board.pop()
-                leaf.append((sign*evl,move))
-            ans = list(max(leaf))
+			leaf = []
+			for (move,fen) in movelist:
+				root_board.push(move)
+				evl = eval_board(root_board)
+				root_board.pop()
+				leaf.append((sign*evl,move))
+			ans = list(max(leaf))
 
-        else:
-            ansl = [(-1*t[0],t[1:]) for t in [_parse_tree(subtree,depth=depth+1)+[move] for (move, subtree) in movelist]]
-            ans = max(ansl)
-            ans = [ans[0]]+ans[1]
+		else:
+			ansl = [(-1*t[0],t[1:]) for t in [_parse_tree(subtree,depth=depth+1)+[move] for (move, subtree) in movelist]]
+			ans = max(ansl)
+			ans = [ans[0]]+ans[1]
 
-    else:
+	else:
 
-        if root_board.is_checkmate():
-            #if debug: print "Looks like a checkmate"
-            ans = list((-1*float('inf'), None))
-        else:
-            #if debug: print "This must be draw"
-            ans = list((0, None))
+		if root_board.is_checkmate():
+			#if debug: print "Looks like a checkmate"
+			ans = list((-1*float('inf'), None))
+		else:
+			#if debug: print "This must be draw"
+			ans = list((0, None))
 
-    #if debug and depth==0:
-    #    for a in sorted(ansl):
-    #        print a
+	#if debug and depth==0:
+	#    for a in sorted(ansl):
+	#        print a
 
-    return ans
-
-
-    
+	return ans
 
